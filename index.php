7b1e4ddea502092b3cd3dede251340c82b4905a3
shine <arinze@winexviv.com>
2018-04-11 23:11:26 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="./css/main.css" media="screen" />
+	<title>Hotel</title>
+</head>
+<body>
+
+<h1 id="caption">WELCOME TO HNG INTERNSHIP 4</h1>
+<p id="love">we love coding</p><br>
+<h1 id='dayOf'><? echo date(' jS \of F Y'); ?></h1>
+<h1 id="dateOf"><? echo date(' h:i A'); ?></h1>
+
+
+</body>
+</html>
